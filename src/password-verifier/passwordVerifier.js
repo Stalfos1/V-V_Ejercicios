a879c0092ca1ed05d4c4ccc27849320f46463d00
on-brulera <luishenryramirezmontalvo746@gmail.com>
2024-01-13 12:24:06 -0500

    Resolución del ejercicio de contraseña

--- src/password-verifier/passwordVerifier.js
+++ src/password-verifier/passwordVerifier.js
@@ -4,47 +4,46 @@ export class PasswordVerifier {
 	#conditionsChecked = 0;
 
 	verify(password) {
+
 		this.#passwordNotNull(password);
 		this.#passwordLength(password);
-		this.#passwordUpperCase(password);
 		this.#passwordLowerCase(password);
+		// if (this.#conditionsChecked == 0) return 'Pasword is incorrect';
+		// if (this.#conditionsChecked >= this.#minimumConditions) return 'Password is correct';
+		this.#passwordUpperCase(password);
+		// if (this.#conditionsChecked >= this.#minimumConditions) return 'Password is correct';
 		this.#passwordNumber(password);
-
-		return 'Password is correct';
+		if (this.#conditionsChecked >= this.#minimumConditions) return 'Password is correct';		
+		return 'Password is incorrect';
 	}
-
-	// #passwordNotNull(password) {
-	// 	try {
-	// 		this.#conditionsChecked++;
-
-	// 		if (!password) throw new Error('Password should not be null');
-	// 		else this.#requiredConditionsCounter++;
-	// 	} catch (error) {
-	// 		if (this.#requiredConditionsCounter <= this.#minimumConditions)
-	// 			this.#requiredConditionsCounter++;
-	// 	}
-	// }
 
 	#passwordNotNull(password) {
 		if (!password) throw new Error('Password should not be null');
-		else this.#requiredConditionsCounter++;
+		this.#requiredConditionsCounter++;
+		this.#conditionsChecked++;
 	}
 
 	#passwordLength(password) {
-		if (password.length <= 8) throw new Error('Password should have more than 8 characters');
+		if (password.length <= 8) throw new Error ('Password should have more than 8 characters');	
+		this.#requiredConditionsCounter++;
+		this.#conditionsChecked++;		
 	}
 
 	#passwordUpperCase(password) {
-		if (!/[A-Z]/.test(password))
-			throw new Error('Password should have at least one uppercase letter');
+		if (!/[A-Z]/.test(password)) throw new  Error( 'Password should have at least one uppercase letter');
+		this.#requiredConditionsCounter++;
+		this.#conditionsChecked++;
 	}
 
 	#passwordLowerCase(password) {
-		if (!/[a-z]/.test(password))
-			throw new Error('Password should have at least one lowercase letter');
+		if (!/[a-z]/.test(password)) throw new Error( 'Password should have at least one lowercase letter');
+		this.#requiredConditionsCounter++;
+		this.#conditionsChecked++;
 	}
 
 	#passwordNumber(password) {
-		if (!/[0-9]/.test(password)) throw new Error('Password should have at least one number');
+		if (!/[0-9]/.test(password)) throw new Error ('Password should have at least one number');
+		this.#requiredConditionsCounter++;
+		this.#conditionsChecked++;
 	}
 }
--- src/password-verifier/passwordVerifier.spec.js
+++ src/password-verifier/passwordVerifier.spec.js
@@ -3,49 +3,45 @@ import { PasswordVerifier } from './passwordVerifier';
 describe('Password verifier', () => {
 	const passwordVerifier = new PasswordVerifier();
 
+	
 	it('should throw error if the password does not have more than 8 characters', () => {
-		const password = 'abcdefg';
-
+		const password = 'abcasd';
 		expect(() => {
 			passwordVerifier.verify(password);
 		}).toThrow('Password should have more than 8 characters');
 	});
-
-	it('should throw error if the password does not have more than 8 characters', () => {
-		const password = null;
-
+	
+	it('should throw error if the password does not have null', () => {
+		const password = '';
 		expect(() => {
 			passwordVerifier.verify(password);
 		}).toThrow('Password should not be null');
 	});
 
 	it('should throw error if the password does not have at least one uppercase letter', () => {
-		const password = 'abcdefghijk';
-
+		const password = 'vidavidaa';
 		expect(() => {
 			passwordVerifier.verify(password);
 		}).toThrow('Password should have at least one uppercase letter');
 	});
 
 	it('should throw error if the password does not have at least one lowercase letter', () => {
-		const password = 'ABCDEFGHIJK';
-
+		const password = 'AAKSJDKASJD';
 		expect(() => {
 			passwordVerifier.verify(password);
 		}).toThrow('Password should have at least one lowercase letter');
 	});
 
 	it('should throw error if the password does not have at least one number', () => {
-		const password = 'AbcDeFgHiJk';
-
+		const password = 'abcasdasaAAA';
 		expect(() => {
 			passwordVerifier.verify(password);
 		}).toThrow('Password should have at least one number');
 	});
 
 	it('should return "Password is correct" if the password is correct', () => {
-		const password = 'AbcDeFgHiJk1';
-
+		const password = 'abcasdasA123';
 		expect(passwordVerifier.verify(password)).toEqual('Password is correct');
 	});
 });
+
